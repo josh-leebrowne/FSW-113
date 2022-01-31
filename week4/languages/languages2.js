3db6618c4a86bd776cc2252fa5be8e81a9fd2fec
josh-leebrowne <90419150+josh-leebrowne@users.noreply.github.com>
2022-01-30 23:11:19 -0600

    Add files via upload

---
+++ week4/languages/languages2.js
@@ -0,0 +1,39 @@
+const lang = 'JavaScript'
+
+// Create an event listener for the submit button that adds all 'input' elements to 
+// a single array using the spread operator. Call the chkLang() function, passing in 
+// the array as an argument.
+
+
+function chkLang() {
+    let result = false
+    let obj = document.querySelector('#TestResult')
+
+    const languages = [
+    document.querySelector('#lang0').value,
+    document.querySelector('#lang1').value,
+    document.querySelector('#lang2').value,
+    document.querySelector('#lang3').value,
+    document.querySelector('#lang4').value,
+    document.querySelector('#lang5').value,
+    document.querySelector('#lang6').value,
+    document.querySelector('#lang7').value,
+    ]
+
+    const javaScript = languages.find(findJS)
+    // use an array method to check whether the user included 'JavaScript' in their
+    // list of languages
+
+    function findJS(item){
+        return item = lang
+    }
+    if (javaScript === lang) {
+        obj.innerText = `Congratulations!\nYou know ${lang}.`
+    }
+    else{
+        obj.innerText = `Sorry,\nyou don't know ${lang}.`
+    }
+    
+}
+const langButton = document.getElementById("submitLang")
+langButton.addEventListener("click", chkLang);
